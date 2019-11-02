9b77e14a328e7c0c1cc5469a7f6bcaba42420af4
likaikai <593496637@qq.com>
2019-11-02 23:23:00 +0800

    axios

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    首页
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #center>
+        <div>
+          购物街
+        </div>
+      </template>
+    </nav-bar>
   </div>
 </template>
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+export default {
+  name: "Home",
+  components: { NavBar },
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+.home-nav {
+  background: var(--color-tint);
+  color: #fff;
+}
+</style>
